fc32c2b073d55637aa631c83744bcdb824dd9a17
Easton <easton.fritsch21@gmail.com>
2023-02-16 12:59:39 -0700

    finished all tasks in Module Project 2 and got 100% on all passes

--- index.js
+++ index.js
@@ -188,11 +188,31 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
+let computer = Math.random();
 
-function game(user, computer){
-  /*add your code here*/
+if (computer <= 0.33){
+  computer = 'rock';
+}else if (computer > 0.33 && computer <= 0.66){
+  computer = 'paper';
+}else if (computer > 0.66){
+  computer = 'scissors';
 }
 
+function game(user, computer){
+  if (user === computer){
+    return "it's a tie";
+  }else if (user === 'rock' && computer === 'scissors'){
+    return "you win!";
+  }else if (user === 'paper' && computer === 'rock'){
+    return "you win!";
+  }else if (user === 'scissors' && computer === 'paper'){
+    return "you win!";
+  }else {
+    return "you lose!";
+  }
+  }
+
+console.log('task 4:', game('paper', computer));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -206,9 +226,12 @@ Using the miles function below do the following:
 3. Return the number of miles
 */
 
-function miles(/*add your code here*/){
-  /*add your code here*/
+
+function miles(km){
+  return km * 0.621371;
 }
+
+console.log('task 5a:', miles(3));
 
 
 
@@ -220,10 +243,11 @@ Using the feet function below do the following:
 3. Return number of feet
 */
 
-function feet(/*add your code here*/){
-  /*add your code here*/
+function feet(cm){
+  return cm / 30.48;
 }
 
+console.log('task 5b:', feet(200));
 
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -240,10 +264,13 @@ Using the annoyingSong function below do the following:
 4. Each time the annoyingSong is run from this loop, it should console.log the string that was returned. 
 */
 
-function annoyingSong(/*add your code here*/){
-      /*add your code here*/
+function annoyingSong(number){
+      for (let i = number; i > 0; i --){
+        return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`;
+      }
 }
 
+console.log('task 6:', annoyingSong(8));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
@@ -260,11 +287,21 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(/*Your Code here */){
-/*Your Code here */
+function grade(score){
+    if (score <= 100 && score >= 90){
+      return 'you got an A';
+    }else if (score <= 89 && score >= 80){
+      return 'you got a B';
+    }else if (score <= 79 && score >= 70){
+      return 'you got a C';
+    }else if (score <= 69 && score >= 60){
+      return 'you got a D';
+    }else {
+      return 'you got an F';
+    }
 }
 
-
+console.log('task 7:', grade(73));
 
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
